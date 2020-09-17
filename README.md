728b00a84599cceb022927df22de462f57f1c2d5
Emeka Ugwuanyi <emylincon@gmail.com>
2020-09-17 16:23:13 +0100

    improved version 5

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Web App for Displaying Sensor data
 * Available on here >>> [link](http://lsbu-sensors.herokuapp.com/)
+
+# Exposed APIs
+## Endpoint for realtime-date
+* This returns a json data
+* `/get-data` 
+
+## Endpoint to obtain data from database
+* This returns a json data
+* `/sensor-data/<int:length>`
+
+## Endpoint to get csv data
+* This returns a csv
+* `/sensor-data/csv/<int:length>`
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template, send_from_directory, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
+from flask_marshmallow import Marshmallow
 import os
 import time
 from datetime import datetime as dt
@@ -9,10 +10,10 @@ import pytz
 
 app = Flask(__name__)
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 # database models
@@ -22,6 +23,15 @@ class Sensors(db.Model):
     datetime = Column(String)
     temperature = Column(Float)
     humidity = Column(Float)
+
+
+class SensorSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'datetime', 'temperature', 'humidity')
+
+
+sensor_schema = SensorSchema()
+sensors_schema = SensorSchema(many=True)
 
 
 @app.cli.command('db_create')
@@ -57,7 +67,7 @@ def db_seed():
     print('database seeded!')
 
 
-#@app.cli.command('db_save')
+# @app.cli.command('db_save')
 def save_data():
     london = pytz.timezone('Europe/London')
     folder = "static/csv_data"
@@ -75,10 +85,10 @@ def save_data():
             out.writerow(row)
 
 
-#@app.cli.command('db_test')
+# @app.cli.command('db_test')
 def delete_rows():
     obj = db.session.query(Sensors).order_by(Sensors.id.desc()).first()
-    all = Sensors.query.limit(obj.id-1).all()
+    all = Sensors.query.limit(obj.id - 1).all()
     for row in all:
         db.session.delete(row)
     db.session.commit()
@@ -94,7 +104,7 @@ def hello_world():
 def add_data(temperature, humidity):
     london = pytz.timezone('Europe/London')
     time_now = dt.now().astimezone(london)
-    raw_save_time = '23:59:57'
+    raw_save_time = '15:59:57'
     save_time = [int(i) for i in raw_save_time.split(':')]
     if (time_now.hour == save_time[0]) and (time_now.minute == save_time[1]) and (time_now.second >= save_time[2]):
         if f'{"{:%d %b %Y}".format(dt.now().astimezone(london))}.csv' not in os.listdir('static/csv_data'):
@@ -116,7 +126,7 @@ def send_data():
         add_data(temperature=float(request.args.get('temperature')), humidity=float(request.args.get('pressure')))
         return jsonify({'info': 'data received'}), 200
     except ValueError:
-        return jsonify({'info': 'Value Error! floats only!'})
+        return jsonify({'info': 'Value Error! floats only!'}), 400
 
 
 @app.route("/download", methods=["POST", "GET"])
@@ -131,5 +141,38 @@ def get_csv():
 def get_data():
     obj = db.session.query(Sensors).order_by(Sensors.id.desc()).first()
     return jsonify({'datetime': obj.datetime, 'temperature': obj.temperature, 'humidity': obj.humidity}), 200
+
+
 # if __name__ == '__main__':
 #     app.run()
+
+
+@app.route("/sensor-data/<int:length>")
+def sensor_data(length=50):
+    result = sensors_schema.dump(Sensors.query.all())[:length]
+    return jsonify(result), 200
+
+
+def lru_cache():
+    folder = "static/temp"
+    files = os.listdir(folder)
+    max_len = 20
+    if len(files) > max_len:
+        files.sort()
+        os.remove(f"{folder}/{files[0]}")
+
+
+@app.route("/sensor-data/csv/<int:length>")
+def sensor_data_csv(length=50):
+    folder = "static/temp"
+    file_name = f'{int(time.time())}.csv'
+    lru_cache()
+    with open(f'{folder}/{file_name}', 'w', newline='\n') as f:
+        out = csv.writer(f)
+        out.writerow(['id', 'datetime', 'temperature', 'humidity'])
+
+        for item in db.session.query(Sensors).all()[-length:]:
+            row = [item.id, item.datetime, item.temperature, item.humidity]
+            out.writerow(row)
+
+    return send_from_directory(folder, filename=file_name, as_attachment=True)
